5250ba6424e236828ffb80799af6a0a7f6fba0f8
Valerie De Leon <43763110+ValerieDeLeon@users.noreply.github.com>
2018-10-23 14:00:17 -0700

    Create DieDriver.java

---
+++ DieDriver.java
@@ -0,0 +1,25 @@
+package die;
+import java.util.Scanner;
+
+public class DieDriver 
+{
+	public static void main (String[] args)
+	{
+		final int TOTAL_ROLLS = 1000;
+		Scanner keyboard = new Scanner(System.in);
+		int roll = 0;
+		
+		System.out.print("Enter the number of sides on your die: ");
+		int sides = keyboard.nextInt();
+		keyboard.close();
+		
+		Die myDie = new Die(sides);
+		
+		for (int i = 1; i <= TOTAL_ROLLS; i++) 
+		{
+			roll = myDie.roll();
+		}
+		
+		System.out.println("Your die has rolled a " + roll + ".");
+	}
+}
